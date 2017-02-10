c0f18f311edf43310304802bdec2e1262708194c
Жалнин Р.В <zhalninrv@lab227.local>
2017-02-10 08:18:25 +0300

    Init

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <cstdlib>
+#include <cstring>
+#include "lab.h"
+#include <iostream>
+
+void print_usage(char* name);
+
+
+int main(int argc, char** argv)
+{
+  if (argc < 3) {
+    print_usage(argv[0]);
+    return 0;
+  }
+
+  lab *l = NULL;
+/*  if (strcmp(argv[1], "arkunovil") == 0) {
+    l = new ivanovii();
+  } else
+   */{
+    print_usage(argv[0]);
+    return 0;
+  }
+
+  l->read_file();
+  l->run(atoi(argv[2]));
+  l->write_result();
+  l->check_result();
+
+  delete l;
+  return 0;
+}
+
+
+void print_usage(char* name)
+{
+  std::cout << "Usage:\n\n  " << name << " <fio> <lab_number>\n";
+}
